e789dd561fc98728c47b11f6744c51cfa7103bc4
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2023-12-27 17:03:16 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -136,6 +136,6 @@ I can't find the <b>{}</b> in my database! 🥲
 
 - ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ᴀɴ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ.
 
-- ꜱᴏᴜʀᴄᴇ - <a href=https://github.com/SL-Bots-TG/Auto-Filter-Bot>ʜᴇʀᴇ</a>
+- ꜱᴏᴜʀᴄᴇ - <a href=https://t.me/Deendayal_dhakad>ʜᴇʀᴇ</a>
 
-ᴅᴇᴠʟᴏᴘᴇʀ - <a href=https://t.me/SL_Bots_Updates>SL_Bots</a>"""
+ᴅᴇᴠʟᴏᴘᴇʀ - <a href=https://t.me/Deendayal_dhakad>Deendayal</a>"""
